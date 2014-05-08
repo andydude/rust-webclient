1471ce2d03e6a1277554e259ea2cade3ecde432e
Andrew Robbins <and_j_rob@yahoo.com>
2014-05-08 00:22:24 -0400

    Fixed tabs

--- src/libwebclient/digest/sha2.rs
+++ src/libwebclient/digest/sha2.rs
@@ -29,70 +29,70 @@ pub static sha256_initial_hash: [u32, .. 8] = [
 ];
 
 pub static sha256_constant_pool: [u32, .. 64] = [
-	0x428a2f98u32, // floor(mod(cbrt(2), 1)*2**32)
-	0x71374491u32, // floor(mod(cbrt(3), 1)*2**32)
-	0xb5c0fbcfu32, // floor(mod(cbrt(5), 1)*2**32)
-	0xe9b5dba5u32, // floor(mod(cbrt(7), 1)*2**32)
-	0x3956c25bu32, // floor(mod(cbrt(11), 1)*2**32)
-	0x59f111f1u32, // floor(mod(cbrt(13), 1)*2**32)
-	0x923f82a4u32, // floor(mod(cbrt(17), 1)*2**32)
-	0xab1c5ed5u32, // floor(mod(cbrt(19), 1)*2**32)
-	0xd807aa98u32, // floor(mod(cbrt(23), 1)*2**32)
-	0x12835b01u32, // floor(mod(cbrt(29), 1)*2**32)
-	0x243185beu32, // floor(mod(cbrt(31), 1)*2**32)
-	0x550c7dc3u32, // floor(mod(cbrt(37), 1)*2**32)
-	0x72be5d74u32, // floor(mod(cbrt(41), 1)*2**32)
-	0x80deb1feu32, // floor(mod(cbrt(43), 1)*2**32)
-	0x9bdc06a7u32, // floor(mod(cbrt(47), 1)*2**32)
-	0xc19bf174u32, // floor(mod(cbrt(53), 1)*2**32)
-	0xe49b69c1u32, // floor(mod(cbrt(59), 1)*2**32)
-	0xefbe4786u32, // floor(mod(cbrt(61), 1)*2**32)
-	0x0fc19dc6u32, // floor(mod(cbrt(67), 1)*2**32)
-	0x240ca1ccu32, // floor(mod(cbrt(71), 1)*2**32)
-	0x2de92c6fu32, // floor(mod(cbrt(73), 1)*2**32)
-	0x4a7484aau32, // floor(mod(cbrt(79), 1)*2**32)
-	0x5cb0a9dcu32, // floor(mod(cbrt(83), 1)*2**32)
-	0x76f988dau32, // floor(mod(cbrt(89), 1)*2**32)
-	0x983e5152u32, // floor(mod(cbrt(97), 1)*2**32)
-	0xa831c66du32, // floor(mod(cbrt(101), 1)*2**32)
-	0xb00327c8u32, // floor(mod(cbrt(103), 1)*2**32)
-	0xbf597fc7u32, // floor(mod(cbrt(107), 1)*2**32)
-	0xc6e00bf3u32, // floor(mod(cbrt(109), 1)*2**32)
-	0xd5a79147u32, // floor(mod(cbrt(113), 1)*2**32)
-	0x06ca6351u32, // floor(mod(cbrt(127), 1)*2**32)
-	0x14292967u32, // floor(mod(cbrt(131), 1)*2**32)
-	0x27b70a85u32, // floor(mod(cbrt(137), 1)*2**32)
-	0x2e1b2138u32, // floor(mod(cbrt(139), 1)*2**32)
-	0x4d2c6dfcu32, // floor(mod(cbrt(149), 1)*2**32)
-	0x53380d13u32, // floor(mod(cbrt(151), 1)*2**32)
-	0x650a7354u32, // floor(mod(cbrt(157), 1)*2**32)
-	0x766a0abbu32, // floor(mod(cbrt(163), 1)*2**32)
-	0x81c2c92eu32, // floor(mod(cbrt(167), 1)*2**32)
-	0x92722c85u32, // floor(mod(cbrt(173), 1)*2**32)
-	0xa2bfe8a1u32, // floor(mod(cbrt(179), 1)*2**32)
-	0xa81a664bu32, // floor(mod(cbrt(181), 1)*2**32)
-	0xc24b8b70u32, // floor(mod(cbrt(191), 1)*2**32)
-	0xc76c51a3u32, // floor(mod(cbrt(193), 1)*2**32)
-	0xd192e819u32, // floor(mod(cbrt(197), 1)*2**32)
-	0xd6990624u32, // floor(mod(cbrt(199), 1)*2**32)
-	0xf40e3585u32, // floor(mod(cbrt(211), 1)*2**32)
-	0x106aa070u32, // floor(mod(cbrt(223), 1)*2**32)
-	0x19a4c116u32, // floor(mod(cbrt(227), 1)*2**32)
-	0x1e376c08u32, // floor(mod(cbrt(229), 1)*2**32)
-	0x2748774cu32, // floor(mod(cbrt(233), 1)*2**32)
-	0x34b0bcb5u32, // floor(mod(cbrt(239), 1)*2**32)
-	0x391c0cb3u32, // floor(mod(cbrt(241), 1)*2**32)
-	0x4ed8aa4au32, // floor(mod(cbrt(251), 1)*2**32)
-	0x5b9cca4fu32, // floor(mod(cbrt(257), 1)*2**32)
-	0x682e6ff3u32, // floor(mod(cbrt(263), 1)*2**32)
-	0x748f82eeu32, // floor(mod(cbrt(269), 1)*2**32)
-	0x78a5636fu32, // floor(mod(cbrt(271), 1)*2**32)
-	0x84c87814u32, // floor(mod(cbrt(277), 1)*2**32)
-	0x8cc70208u32, // floor(mod(cbrt(281), 1)*2**32)
-	0x90befffau32, // floor(mod(cbrt(283), 1)*2**32)
-	0xa4506cebu32, // floor(mod(cbrt(293), 1)*2**32)
-	0xbef9a3f7u32, // floor(mod(cbrt(307), 1)*2**32)
-	0xc67178f2u32  // floor(mod(cbrt(311), 1)*2**32)
+    0x428a2f98u32, // floor(mod(cbrt(2), 1)*2**32)
+    0x71374491u32, // floor(mod(cbrt(3), 1)*2**32)
+    0xb5c0fbcfu32, // floor(mod(cbrt(5), 1)*2**32)
+    0xe9b5dba5u32, // floor(mod(cbrt(7), 1)*2**32)
+    0x3956c25bu32, // floor(mod(cbrt(11), 1)*2**32)
+    0x59f111f1u32, // floor(mod(cbrt(13), 1)*2**32)
+    0x923f82a4u32, // floor(mod(cbrt(17), 1)*2**32)
+    0xab1c5ed5u32, // floor(mod(cbrt(19), 1)*2**32)
+    0xd807aa98u32, // floor(mod(cbrt(23), 1)*2**32)
+    0x12835b01u32, // floor(mod(cbrt(29), 1)*2**32)
+    0x243185beu32, // floor(mod(cbrt(31), 1)*2**32)
+    0x550c7dc3u32, // floor(mod(cbrt(37), 1)*2**32)
+    0x72be5d74u32, // floor(mod(cbrt(41), 1)*2**32)
+    0x80deb1feu32, // floor(mod(cbrt(43), 1)*2**32)
+    0x9bdc06a7u32, // floor(mod(cbrt(47), 1)*2**32)
+    0xc19bf174u32, // floor(mod(cbrt(53), 1)*2**32)
+    0xe49b69c1u32, // floor(mod(cbrt(59), 1)*2**32)
+    0xefbe4786u32, // floor(mod(cbrt(61), 1)*2**32)
+    0x0fc19dc6u32, // floor(mod(cbrt(67), 1)*2**32)
+    0x240ca1ccu32, // floor(mod(cbrt(71), 1)*2**32)
+    0x2de92c6fu32, // floor(mod(cbrt(73), 1)*2**32)
+    0x4a7484aau32, // floor(mod(cbrt(79), 1)*2**32)
+    0x5cb0a9dcu32, // floor(mod(cbrt(83), 1)*2**32)
+    0x76f988dau32, // floor(mod(cbrt(89), 1)*2**32)
+    0x983e5152u32, // floor(mod(cbrt(97), 1)*2**32)
+    0xa831c66du32, // floor(mod(cbrt(101), 1)*2**32)
+    0xb00327c8u32, // floor(mod(cbrt(103), 1)*2**32)
+    0xbf597fc7u32, // floor(mod(cbrt(107), 1)*2**32)
+    0xc6e00bf3u32, // floor(mod(cbrt(109), 1)*2**32)
+    0xd5a79147u32, // floor(mod(cbrt(113), 1)*2**32)
+    0x06ca6351u32, // floor(mod(cbrt(127), 1)*2**32)
+    0x14292967u32, // floor(mod(cbrt(131), 1)*2**32)
+    0x27b70a85u32, // floor(mod(cbrt(137), 1)*2**32)
+    0x2e1b2138u32, // floor(mod(cbrt(139), 1)*2**32)
+    0x4d2c6dfcu32, // floor(mod(cbrt(149), 1)*2**32)
+    0x53380d13u32, // floor(mod(cbrt(151), 1)*2**32)
+    0x650a7354u32, // floor(mod(cbrt(157), 1)*2**32)
+    0x766a0abbu32, // floor(mod(cbrt(163), 1)*2**32)
+    0x81c2c92eu32, // floor(mod(cbrt(167), 1)*2**32)
+    0x92722c85u32, // floor(mod(cbrt(173), 1)*2**32)
+    0xa2bfe8a1u32, // floor(mod(cbrt(179), 1)*2**32)
+    0xa81a664bu32, // floor(mod(cbrt(181), 1)*2**32)
+    0xc24b8b70u32, // floor(mod(cbrt(191), 1)*2**32)
+    0xc76c51a3u32, // floor(mod(cbrt(193), 1)*2**32)
+    0xd192e819u32, // floor(mod(cbrt(197), 1)*2**32)
+    0xd6990624u32, // floor(mod(cbrt(199), 1)*2**32)
+    0xf40e3585u32, // floor(mod(cbrt(211), 1)*2**32)
+    0x106aa070u32, // floor(mod(cbrt(223), 1)*2**32)
+    0x19a4c116u32, // floor(mod(cbrt(227), 1)*2**32)
+    0x1e376c08u32, // floor(mod(cbrt(229), 1)*2**32)
+    0x2748774cu32, // floor(mod(cbrt(233), 1)*2**32)
+    0x34b0bcb5u32, // floor(mod(cbrt(239), 1)*2**32)
+    0x391c0cb3u32, // floor(mod(cbrt(241), 1)*2**32)
+    0x4ed8aa4au32, // floor(mod(cbrt(251), 1)*2**32)
+    0x5b9cca4fu32, // floor(mod(cbrt(257), 1)*2**32)
+    0x682e6ff3u32, // floor(mod(cbrt(263), 1)*2**32)
+    0x748f82eeu32, // floor(mod(cbrt(269), 1)*2**32)
+    0x78a5636fu32, // floor(mod(cbrt(271), 1)*2**32)
+    0x84c87814u32, // floor(mod(cbrt(277), 1)*2**32)
+    0x8cc70208u32, // floor(mod(cbrt(281), 1)*2**32)
+    0x90befffau32, // floor(mod(cbrt(283), 1)*2**32)
+    0xa4506cebu32, // floor(mod(cbrt(293), 1)*2**32)
+    0xbef9a3f7u32, // floor(mod(cbrt(307), 1)*2**32)
+    0xc67178f2u32  // floor(mod(cbrt(311), 1)*2**32)
 ];
 
 pub static sha384_initial_hash: [u64, .. 8] = [
@@ -140,86 +140,86 @@ pub static sha512256_initial_hash: [u64, .. 8] = [
 ];
 
 pub static sha512_constant_pool: [u64, .. 80] = [
-	0x428a2f98d728ae22u64, // floor(mod(cbrt(2), 1)*2**64)
-	0x7137449123ef65cdu64, // floor(mod(cbrt(3), 1)*2**64)
-	0xb5c0fbcfec4d3b2fu64, // floor(mod(cbrt(5), 1)*2**64)
-	0xe9b5dba58189dbbcu64, // floor(mod(cbrt(7), 1)*2**64)
-	0x3956c25bf348b538u64, // floor(mod(cbrt(11), 1)*2**64)
-	0x59f111f1b605d019u64, // floor(mod(cbrt(13), 1)*2**64)
-	0x923f82a4af194f9bu64, // floor(mod(cbrt(17), 1)*2**64)
-	0xab1c5ed5da6d8118u64, // floor(mod(cbrt(19), 1)*2**64)
-	0xd807aa98a3030242u64, // floor(mod(cbrt(23), 1)*2**64)
-	0x12835b0145706fbeu64, // floor(mod(cbrt(29), 1)*2**64)
-	0x243185be4ee4b28cu64, // floor(mod(cbrt(31), 1)*2**64)
-	0x550c7dc3d5ffb4e2u64, // floor(mod(cbrt(37), 1)*2**64)
-	0x72be5d74f27b896fu64, // floor(mod(cbrt(41), 1)*2**64)
-	0x80deb1fe3b1696b1u64, // floor(mod(cbrt(43), 1)*2**64)
-	0x9bdc06a725c71235u64, // floor(mod(cbrt(47), 1)*2**64)
-	0xc19bf174cf692694u64, // floor(mod(cbrt(53), 1)*2**64)
-	0xe49b69c19ef14ad2u64, // floor(mod(cbrt(59), 1)*2**64)
-	0xefbe4786384f25e3u64, // floor(mod(cbrt(61), 1)*2**64)
-	0x0fc19dc68b8cd5b5u64, // floor(mod(cbrt(67), 1)*2**64)
-	0x240ca1cc77ac9c65u64, // floor(mod(cbrt(71), 1)*2**64)
-	0x2de92c6f592b0275u64, // floor(mod(cbrt(73), 1)*2**64)
-	0x4a7484aa6ea6e483u64, // floor(mod(cbrt(79), 1)*2**64)
-	0x5cb0a9dcbd41fbd4u64, // floor(mod(cbrt(83), 1)*2**64)
-	0x76f988da831153b5u64, // floor(mod(cbrt(89), 1)*2**64)
-	0x983e5152ee66dfabu64, // floor(mod(cbrt(97), 1)*2**64)
-	0xa831c66d2db43210u64, // floor(mod(cbrt(101), 1)*2**64)
-	0xb00327c898fb213fu64, // floor(mod(cbrt(103), 1)*2**64)
-	0xbf597fc7beef0ee4u64, // floor(mod(cbrt(107), 1)*2**64)
-	0xc6e00bf33da88fc2u64, // floor(mod(cbrt(109), 1)*2**64)
-	0xd5a79147930aa725u64, // floor(mod(cbrt(113), 1)*2**64)
-	0x06ca6351e003826fu64, // floor(mod(cbrt(127), 1)*2**64)
-	0x142929670a0e6e70u64, // floor(mod(cbrt(131), 1)*2**64)
-	0x27b70a8546d22ffcu64, // floor(mod(cbrt(137), 1)*2**64)
-	0x2e1b21385c26c926u64, // floor(mod(cbrt(139), 1)*2**64)
-	0x4d2c6dfc5ac42aedu64, // floor(mod(cbrt(149), 1)*2**64)
-	0x53380d139d95b3dfu64, // floor(mod(cbrt(151), 1)*2**64)
-	0x650a73548baf63deu64, // floor(mod(cbrt(157), 1)*2**64)
-	0x766a0abb3c77b2a8u64, // floor(mod(cbrt(163), 1)*2**64)
-	0x81c2c92e47edaee6u64, // floor(mod(cbrt(167), 1)*2**64)
-	0x92722c851482353bu64, // floor(mod(cbrt(173), 1)*2**64)
-	0xa2bfe8a14cf10364u64, // floor(mod(cbrt(179), 1)*2**64)
-	0xa81a664bbc423001u64, // floor(mod(cbrt(181), 1)*2**64)
-	0xc24b8b70d0f89791u64, // floor(mod(cbrt(191), 1)*2**64)
-	0xc76c51a30654be30u64, // floor(mod(cbrt(193), 1)*2**64)
-	0xd192e819d6ef5218u64, // floor(mod(cbrt(197), 1)*2**64)
-	0xd69906245565a910u64, // floor(mod(cbrt(199), 1)*2**64)
-	0xf40e35855771202au64, // floor(mod(cbrt(211), 1)*2**64)
-	0x106aa07032bbd1b8u64, // floor(mod(cbrt(223), 1)*2**64)
-	0x19a4c116b8d2d0c8u64, // floor(mod(cbrt(227), 1)*2**64)
-	0x1e376c085141ab53u64, // floor(mod(cbrt(229), 1)*2**64)
-	0x2748774cdf8eeb99u64, // floor(mod(cbrt(233), 1)*2**64)
-	0x34b0bcb5e19b48a8u64, // floor(mod(cbrt(239), 1)*2**64)
-	0x391c0cb3c5c95a63u64, // floor(mod(cbrt(241), 1)*2**64)
-	0x4ed8aa4ae3418acbu64, // floor(mod(cbrt(251), 1)*2**64)
-	0x5b9cca4f7763e373u64, // floor(mod(cbrt(257), 1)*2**64)
-	0x682e6ff3d6b2b8a3u64, // floor(mod(cbrt(263), 1)*2**64)
-	0x748f82ee5defb2fcu64, // floor(mod(cbrt(269), 1)*2**64)
-	0x78a5636f43172f60u64, // floor(mod(cbrt(271), 1)*2**64)
-	0x84c87814a1f0ab72u64, // floor(mod(cbrt(277), 1)*2**64)
-	0x8cc702081a6439ecu64, // floor(mod(cbrt(281), 1)*2**64)
-	0x90befffa23631e28u64, // floor(mod(cbrt(283), 1)*2**64)
-	0xa4506cebde82bde9u64, // floor(mod(cbrt(293), 1)*2**64)
-	0xbef9a3f7b2c67915u64, // floor(mod(cbrt(307), 1)*2**64)
-	0xc67178f2e372532bu64, // floor(mod(cbrt(311), 1)*2**64)
-	0xca273eceea26619cu64, //
-	0xd186b8c721c0c207u64, //
-	0xeada7dd6cde0eb1eu64, //
-	0xf57d4f7fee6ed178u64, //
-	0x06f067aa72176fbau64, //
-	0x0a637dc5a2c898a6u64, //
-	0x113f9804bef90daeu64, //
-	0x1b710b35131c471bu64, //
-	0x28db77f523047d84u64, //
-	0x32caab7b40c72493u64, //
-	0x3c9ebe0a15c9bebcu64, //
-	0x431d67c49c100d4cu64, //
-	0x4cc5d4becb3e42b6u64, //
-	0x597f299cfc657e2au64, //
-	0x5fcb6fab3ad6faecu64, //
-	0x6c44198c4a475817u64  //
+    0x428a2f98d728ae22u64, // floor(mod(cbrt(2), 1)*2**64)
+    0x7137449123ef65cdu64, // floor(mod(cbrt(3), 1)*2**64)
+    0xb5c0fbcfec4d3b2fu64, // floor(mod(cbrt(5), 1)*2**64)
+    0xe9b5dba58189dbbcu64, // floor(mod(cbrt(7), 1)*2**64)
+    0x3956c25bf348b538u64, // floor(mod(cbrt(11), 1)*2**64)
+    0x59f111f1b605d019u64, // floor(mod(cbrt(13), 1)*2**64)
+    0x923f82a4af194f9bu64, // floor(mod(cbrt(17), 1)*2**64)
+    0xab1c5ed5da6d8118u64, // floor(mod(cbrt(19), 1)*2**64)
+    0xd807aa98a3030242u64, // floor(mod(cbrt(23), 1)*2**64)
+    0x12835b0145706fbeu64, // floor(mod(cbrt(29), 1)*2**64)
+    0x243185be4ee4b28cu64, // floor(mod(cbrt(31), 1)*2**64)
+    0x550c7dc3d5ffb4e2u64, // floor(mod(cbrt(37), 1)*2**64)
+    0x72be5d74f27b896fu64, // floor(mod(cbrt(41), 1)*2**64)
+    0x80deb1fe3b1696b1u64, // floor(mod(cbrt(43), 1)*2**64)
+    0x9bdc06a725c71235u64, // floor(mod(cbrt(47), 1)*2**64)
+    0xc19bf174cf692694u64, // floor(mod(cbrt(53), 1)*2**64)
+    0xe49b69c19ef14ad2u64, // floor(mod(cbrt(59), 1)*2**64)
+    0xefbe4786384f25e3u64, // floor(mod(cbrt(61), 1)*2**64)
+    0x0fc19dc68b8cd5b5u64, // floor(mod(cbrt(67), 1)*2**64)
+    0x240ca1cc77ac9c65u64, // floor(mod(cbrt(71), 1)*2**64)
+    0x2de92c6f592b0275u64, // floor(mod(cbrt(73), 1)*2**64)
+    0x4a7484aa6ea6e483u64, // floor(mod(cbrt(79), 1)*2**64)
+    0x5cb0a9dcbd41fbd4u64, // floor(mod(cbrt(83), 1)*2**64)
+    0x76f988da831153b5u64, // floor(mod(cbrt(89), 1)*2**64)
+    0x983e5152ee66dfabu64, // floor(mod(cbrt(97), 1)*2**64)
+    0xa831c66d2db43210u64, // floor(mod(cbrt(101), 1)*2**64)
+    0xb00327c898fb213fu64, // floor(mod(cbrt(103), 1)*2**64)
+    0xbf597fc7beef0ee4u64, // floor(mod(cbrt(107), 1)*2**64)
+    0xc6e00bf33da88fc2u64, // floor(mod(cbrt(109), 1)*2**64)
+    0xd5a79147930aa725u64, // floor(mod(cbrt(113), 1)*2**64)
+    0x06ca6351e003826fu64, // floor(mod(cbrt(127), 1)*2**64)
+    0x142929670a0e6e70u64, // floor(mod(cbrt(131), 1)*2**64)
+    0x27b70a8546d22ffcu64, // floor(mod(cbrt(137), 1)*2**64)
+    0x2e1b21385c26c926u64, // floor(mod(cbrt(139), 1)*2**64)
+    0x4d2c6dfc5ac42aedu64, // floor(mod(cbrt(149), 1)*2**64)
+    0x53380d139d95b3dfu64, // floor(mod(cbrt(151), 1)*2**64)
+    0x650a73548baf63deu64, // floor(mod(cbrt(157), 1)*2**64)
+    0x766a0abb3c77b2a8u64, // floor(mod(cbrt(163), 1)*2**64)
+    0x81c2c92e47edaee6u64, // floor(mod(cbrt(167), 1)*2**64)
+    0x92722c851482353bu64, // floor(mod(cbrt(173), 1)*2**64)
+    0xa2bfe8a14cf10364u64, // floor(mod(cbrt(179), 1)*2**64)
+    0xa81a664bbc423001u64, // floor(mod(cbrt(181), 1)*2**64)
+    0xc24b8b70d0f89791u64, // floor(mod(cbrt(191), 1)*2**64)
+    0xc76c51a30654be30u64, // floor(mod(cbrt(193), 1)*2**64)
+    0xd192e819d6ef5218u64, // floor(mod(cbrt(197), 1)*2**64)
+    0xd69906245565a910u64, // floor(mod(cbrt(199), 1)*2**64)
+    0xf40e35855771202au64, // floor(mod(cbrt(211), 1)*2**64)
+    0x106aa07032bbd1b8u64, // floor(mod(cbrt(223), 1)*2**64)
+    0x19a4c116b8d2d0c8u64, // floor(mod(cbrt(227), 1)*2**64)
+    0x1e376c085141ab53u64, // floor(mod(cbrt(229), 1)*2**64)
+    0x2748774cdf8eeb99u64, // floor(mod(cbrt(233), 1)*2**64)
+    0x34b0bcb5e19b48a8u64, // floor(mod(cbrt(239), 1)*2**64)
+    0x391c0cb3c5c95a63u64, // floor(mod(cbrt(241), 1)*2**64)
+    0x4ed8aa4ae3418acbu64, // floor(mod(cbrt(251), 1)*2**64)
+    0x5b9cca4f7763e373u64, // floor(mod(cbrt(257), 1)*2**64)
+    0x682e6ff3d6b2b8a3u64, // floor(mod(cbrt(263), 1)*2**64)
+    0x748f82ee5defb2fcu64, // floor(mod(cbrt(269), 1)*2**64)
+    0x78a5636f43172f60u64, // floor(mod(cbrt(271), 1)*2**64)
+    0x84c87814a1f0ab72u64, // floor(mod(cbrt(277), 1)*2**64)
+    0x8cc702081a6439ecu64, // floor(mod(cbrt(281), 1)*2**64)
+    0x90befffa23631e28u64, // floor(mod(cbrt(283), 1)*2**64)
+    0xa4506cebde82bde9u64, // floor(mod(cbrt(293), 1)*2**64)
+    0xbef9a3f7b2c67915u64, // floor(mod(cbrt(307), 1)*2**64)
+    0xc67178f2e372532bu64, // floor(mod(cbrt(311), 1)*2**64)
+    0xca273eceea26619cu64, //
+    0xd186b8c721c0c207u64, //
+    0xeada7dd6cde0eb1eu64, //
+    0xf57d4f7fee6ed178u64, //
+    0x06f067aa72176fbau64, //
+    0x0a637dc5a2c898a6u64, //
+    0x113f9804bef90daeu64, //
+    0x1b710b35131c471bu64, //
+    0x28db77f523047d84u64, //
+    0x32caab7b40c72493u64, //
+    0x3c9ebe0a15c9bebcu64, //
+    0x431d67c49c100d4cu64, //
+    0x4cc5d4becb3e42b6u64, //
+    0x597f299cfc657e2au64, //
+    0x5fcb6fab3ad6faecu64, //
+    0x6c44198c4a475817u64  //
 ];
 
 //
@@ -564,7 +564,7 @@ pub fn sha512_update(hash: &mut[u64], m: &[u8]) {
 
 pub struct SHA224 {
     msg_size: uint,
-	state: ~[u32]
+    state: ~[u32]
 }
 
 impl HashAlgorithm for SHA224 {
@@ -599,11 +599,11 @@ impl HashAlgorithm for SHA224 {
         self.state = sha224_begin();
     }
 
-	fn hash_block(&mut self, msg_block: &[u8]) {
+    fn hash_block(&mut self, msg_block: &[u8]) {
         sha256_update(self.state, msg_block);
     }
 
-	fn hash_last_block(&mut self, msg_piece: &[u8]) {
+    fn hash_last_block(&mut self, msg_piece: &[u8]) {
         let m = u64::pad_be_64(msg_piece, 0x80u8, self.msg_size);
         for block in m.chunks(64) {
             self.hash_block(block);
@@ -621,7 +621,7 @@ pub fn sha224_hash(msg: &[u8]) -> ~[u8] {
 
 pub struct SHA256 {
     msg_size: uint,
-	state: ~[u32]
+    state: ~[u32]
 }
 
 impl HashAlgorithm for SHA256 {
@@ -656,11 +656,11 @@ impl HashAlgorithm for SHA256 {
         self.state = sha256_begin();
     }
 
-	fn hash_block(&mut self, msg_block: &[u8]) {
+    fn hash_block(&mut self, msg_block: &[u8]) {
         sha256_update(self.state, msg_block);
     }
 
-	fn hash_last_block(&mut self, msg_piece: &[u8]) {
+    fn hash_last_block(&mut self, msg_piece: &[u8]) {
         let m = u64::pad_be_64(msg_piece, 0x80u8, self.msg_size);
         for block in m.chunks(64) {
             self.hash_block(block);
@@ -678,7 +678,7 @@ pub fn sha256_hash(msg: &[u8]) -> ~[u8] {
 
 pub struct SHA384 {
     msg_size: uint,
-	state: ~[u64]
+    state: ~[u64]
 }
 
 impl HashAlgorithm for SHA384 {
@@ -713,11 +713,11 @@ impl HashAlgorithm for SHA384 {
         self.state = sha384_begin();
     }
 
-	fn hash_block(&mut self, msg_block: &[u8]) {
+    fn hash_block(&mut self, msg_block: &[u8]) {
         sha512_update(self.state, msg_block);
     }
 
-	fn hash_last_block(&mut self, msg_piece: &[u8]) {
+    fn hash_last_block(&mut self, msg_piece: &[u8]) {
         let m = u64::pad_be_128(msg_piece, 0x80u8, self.msg_size);
         for block in m.chunks(128) {
             self.hash_block(block);
@@ -735,7 +735,7 @@ pub fn sha384_hash(msg: &[u8]) -> ~[u8] {
 
 pub struct SHA512 {
     msg_size: uint,
-	state: ~[u64]
+    state: ~[u64]
 }
 
 impl HashAlgorithm for SHA512 {
@@ -774,7 +774,7 @@ impl HashAlgorithm for SHA512 {
         sha512_update(self.state, msg_block);
     }
 
-	fn hash_last_block(&mut self, msg_piece: &[u8]) {
+    fn hash_last_block(&mut self, msg_piece: &[u8]) {
         let m = u64::pad_be_128(msg_piece, 0x80u8, self.msg_size);
         for block in m.chunks(128) {
             self.hash_block(block);
@@ -792,7 +792,7 @@ pub fn sha512_hash(msg: &[u8]) -> ~[u8] {
 
 pub struct SHA512224 {
     msg_size: uint,
-	state: ~[u64]
+    state: ~[u64]
 }
 
 impl HashAlgorithm for SHA512224 {
@@ -827,11 +827,11 @@ impl HashAlgorithm for SHA512224 {
         self.state = sha512224_begin();
     }
 
-	fn hash_block(&mut self, msg_block: &[u8]) {
+    fn hash_block(&mut self, msg_block: &[u8]) {
         sha512_update(self.state, msg_block);
     }
 
-	fn hash_last_block(&mut self, msg_piece: &[u8]) {
+    fn hash_last_block(&mut self, msg_piece: &[u8]) {
         let m = u64::pad_be_128(msg_piece, 0x80u8, self.msg_size);
         for block in m.chunks(128) {
             self.hash_block(block);
@@ -849,7 +849,7 @@ pub fn sha512224_hash(msg: &[u8]) -> ~[u8] {
 
 pub struct SHA512256 {
     msg_size: uint,
-	state: ~[u64]
+    state: ~[u64]
 }
 
 impl HashAlgorithm for SHA512256 {
@@ -884,11 +884,11 @@ impl HashAlgorithm for SHA512256 {
         self.state = sha512256_begin();
     }
 
-	fn hash_block(&mut self, msg_block: &[u8]) {
+    fn hash_block(&mut self, msg_block: &[u8]) {
         sha512_update(self.state, msg_block);
     }
 
-	fn hash_last_block(&mut self, msg_piece: &[u8]) {
+    fn hash_last_block(&mut self, msg_piece: &[u8]) {
         let m = u64::pad_be_128(msg_piece, 0x80u8, self.msg_size);
         for block in m.chunks(128) {
             self.hash_block(block);
